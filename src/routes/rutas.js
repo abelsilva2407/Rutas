f17574af1eef0768bd75a4399a6fed76e07def7b
abelsilva2407 <42190156+abelsilva2407@users.noreply.github.com>
2021-03-11 12:23:35 -0800

    Se agregaron las rutas de reportes y se agregaron datos de meses pasados

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -100,17 +100,124 @@ router.put('/participacion/:idAlumno', (req, res) => {
     res.status(200).json(alumnos);
 });
 
+//Te da el reporte semanal de cierto Alumno
+router.get('/reporte/semanal/:idAlumno', (req, res) => {
+    const {idAlumno} = req.params;
+    alumnos.forEach(alumno => {
+        if (alumno.id == idAlumno) {
+            // Se calcula la fecha de la semana pasada
+            let lastWeek = new Date();
+            lastWeek.setDate(lastWeek.getDate()-7);
+            lastWeek.setHours(0,0,0,0);
+            // Se filtran todos los datos en asistencias, tareas y participaciones
+            for (const fechaStr in alumno.asistencias) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastWeek) delete alumno.asistencias[fechaStr];
+            }
 
-// router.post('/', (req, res) => {
-//     const {title, version, genre} = req.body;
-//     if (title && version && genre) {
-//         const id = juegos.length + 1;
-//         const nuevoJuego = {...req.body, id};
-//         juegos.push(nuevoJuego);
-//         res.status(200).json(juegos);
-//     } else {
-//         res.status(500).json({error:"no data"});
-//     }
-// });
+            for (const fechaStr in alumno.tareas) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastWeek) delete alumno.tareas[fechaStr];
+            }
+
+            for (const fechaStr in alumno.participaciones) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastWeek) delete alumno.participaciones[fechaStr];
+            }
+            res.json(alumno);
+        }
+    })
+});
+
+
+//Te da el reporte mensual de cierto Alumno
+router.get('/reporte/mensual/:idAlumno', (req, res) => {
+    const {idAlumno} = req.params;
+    alumnos.forEach(alumno => {
+        if (alumno.id == idAlumno) {
+            // Se calcula la fecha del mes pasado
+            let lastMonth = new Date();
+            lastMonth.setMonth(lastMonth.getMonth() -1);
+            lastMonth.setHours(0,0,0,0);
+            // Se filtran todos los datos en asistencias, tareas y participaciones
+            for (const fechaStr in alumno.asistencias) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastMonth) delete alumno.asistencias[fechaStr];
+            }
+
+            for (const fechaStr in alumno.tareas) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastMonth) delete alumno.tareas[fechaStr];
+            }
+
+            for (const fechaStr in alumno.participaciones) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha<= lastMonth) delete alumno.participaciones[fechaStr];
+            }
+            res.json(alumno);
+        }
+    })
+});
+
+
+
+//Te da el reporte en un rango de fechas de cierto Alumno
+router.get('/reporte/:fecha1/:fecha2/:idAlumno', (req, res) => {
+    const {fecha1, fecha2, idAlumno} = req.params;
+    alumnos.forEach(alumno => {
+        if (alumno.id == idAlumno) {
+            //Se crean objetos de tipo Date a partir de los strings
+            const dateParts1 = fecha1.split('-');
+            const dateParts2 = fecha2.split('-');
+            const fechaI = new Date(+dateParts1[0], dateParts1[1]-1, +dateParts1[2]);
+            const fechaF = new Date(+dateParts2[0], dateParts2[1]-1, +dateParts2[2]);
+
+            // Se filtran todos los datos en asistencias, tareas y participaciones
+            for (const fechaStr in alumno.asistencias) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha< fechaI || fecha>fechaF) delete alumno.asistencias[fechaStr];
+            }
+
+            for (const fechaStr in alumno.tareas) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha< fechaI || fecha>fechaF) delete alumno.tareas[fechaStr];
+            }
+
+            for (const fechaStr in alumno.participaciones) {
+                //Nuestros strings estan en dd/mm/yyyy pero JS los crea de manera yyyy/mm/dd
+                const dateParts = fechaStr.split('/');
+                const fecha = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+                fecha.setHours(0,0,0,0);
+                if (fecha< fechaI || fecha>fechaF) delete alumno.participaciones[fechaStr];
+            }
+            res.json(alumno);
+        }
+    })
+});
+
 
 module.exports = router;
